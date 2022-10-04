a49a0d4f1c5c1a5c586fcfc144a5986022ea8eec
KillerK1998 <kaler.sappington@gmail.com>
2022-10-03 20:34:15 -0600

    added some elements to director class

--- root/hilo/game/Director.py
+++ root/hilo/game/Director.py
@@ -11,5 +11,43 @@ class Director:
         self.score = 300
         self.total_score = 0
 
+    def get_inputs(self):
+        #asks the user if they choose a higher number or a lower number.
+
+
+        #Args:
+            #self(director):An instance of Director
+
+        num_select=input("High or low? [h/l]")
+        self.is_playing = input(num_select == "h")
+        self.is_playing = input(num_select == "l")
+
+    def do_updates(self):
+        #draws a card
+        #Args
+            #self(Director) An instance of Director
+
+        if not self.is_playing:
+            return
+        
+        for i in range(len(self.card)):
+            card = self.card
+            card.draw()
+            self.score += card.points
+        self.total_score += self.score
+    
+    def do_outputs(self):
+        #display results
+
+        #Args:
+            #self(Director): An instance of Director
+
+            if not self.is_playing:
+                return
+
+            values = "" 
+            for i in range(len(self.card)):
+
+
         
 
