aaafeccd9c5f8aac269e039057600be5f6cda888
Ejpulver <pul15004@byui.edu>
2022-10-03 20:18:03 -0600

    added code to dealer and director classes

--- root/hilo/game/Director.py
+++ root/hilo/game/Director.py
@@ -1,5 +1,5 @@
 from operator import truediv
-from game.dealer import Dealer
+from game.dealer import dealer
 
 class Director:
     # A person who directs the game.
@@ -9,7 +9,29 @@ class Director:
     def __init__(self):
         self.is_playing = True
         self.score = 300
-        self.total_score = 0
+        self.total_score = 300
+
+    def start_game(self):
+
+        while self.is_playing:
+            self.get_inputs()
+            self.do_updates()
+            self.do_outputs()
+
+    def get_inputs(self):
+
+        pass
+
+    def do_updates(self):
+
+        pass
+
+    def do_outputs(self):
+        
+        pass
+
+
+
 
         
 
