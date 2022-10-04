c69529357078f09b58d26a4d76fc54d78acb6d94
Ejpulver <pul15004@byui.edu>
2022-10-03 20:18:46 -0600

    same as last

--- root/hilo/game/Dealer.py
+++ root/hilo/game/Dealer.py
@@ -1,3 +1,22 @@
 import random
 
+class Dealer:
+
+    def __init__(self):
+
+        self.points = 0
+        self.value = 0
+        self.user_guess = 0
+
+    
+    def deal(self):
+
+        self.value = random.randint(1, 13)
+        if self.user_guess > self.value:
+            self.points += 100
+        elif self.user_guess < self.value:
+            self.points -= 75
+        else:
+            self.points += 0
+        
 
