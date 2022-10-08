f036f33a2c7aaf5d2a6798203b9246a54e1a594a
KillerK1998 <kaler.sappington@gmail.com>
2022-10-07 19:15:57 -0600

    added comments

--- root/hilo/game/Dealer.py
+++ root/hilo/game/Dealer.py
@@ -5,8 +5,9 @@ class Dealer:
     def __init__(self):
         self.card = 0
         self.deal()
-
+#starting cards is = to zero
     def deal(self):
         self.card = random.randint(1, 13)
+        #random card is picked between 1 and 13
         
 
